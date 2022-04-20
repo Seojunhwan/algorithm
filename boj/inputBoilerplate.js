bc9039e6ba2b2ac5de0558dd5c43cfc9db2d9be6
SeoJunhwan <seojunhwan@kakao.com>
2022-04-20 14:22:15 +0900

    boj node.js 용 보일러플레이트

---
+++ boj/inputBoilerplate.js
@@ -0,0 +1,14 @@
+// inputBoilerplate
+const { readFileSync } = require("fs");
+
+const stdin = readFileSync(
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/example.txt"
+)
+  .toString()
+  .trim()
+  .split("\n");
+
+const input = (() => {
+  let i = 0;
+  return () => stdin[i++];
+})();
