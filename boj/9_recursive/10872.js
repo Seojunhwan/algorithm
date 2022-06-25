3009e3d348a9c817d34c3644d30f638e69b51ea3
Seojunhwan <seojunhwan@kakao.com>
2022-06-25 17:35:57 +0900

    boj snippets 작성

---
+++ boj/9_recursive/10872.js
@@ -0,0 +1,13 @@
+const { readFileSync } = require("fs");
+
+const stdin = readFileSync(
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/example.txt"
+)
+  .toString()
+  .trim()
+  .split("\n");
+
+const input = (() => {
+  let i = 0;
+  return () => stdin[i++];
+})();
